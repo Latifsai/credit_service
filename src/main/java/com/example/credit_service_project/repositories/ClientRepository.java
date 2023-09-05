b5720a599109fbffcf7d05d51860909f80407b58
Latifus <saitbaevlatif@gmail.com>
2023-09-05 10:34:05 +0200

    commit "create credit"

--- src/main/java/com/example/credit_service_project/repositories/ClientRepository.java
+++ src/main/java/com/example/credit_service_project/repositories/ClientRepository.java
@@ -3,7 +3,9 @@ package com.example.credit_service_project.repositories;
 import com.example.credit_service_project.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+    Optional<Client> findByName(String name);
 }
