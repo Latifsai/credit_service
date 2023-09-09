3f33f815a2332c5f5f488bfa6d7a6f51cc311c14
Latifus <saitbaevlatif@gmail.com>
2023-09-09 09:11:33 +0200

    commit "create credit"

--- src/test/java/com/example/credit_service_project/services/auth/JwtTokenProviderTest.java
+++ src/test/java/com/example/credit_service_project/services/auth/JwtTokenProviderTest.java
@@ -1,15 +1,16 @@
 package com.example.credit_service_project.services.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.Duration;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -34,7 +35,18 @@ class JwtTokenProviderTest {
     void generateToken() {
         String username = "Olga";
         String token = provider.generateToken(username);
+
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        Date issuedAt = claims.getIssuedAt();
+        Date expiration = claims.getExpiration();
+
+        assertEquals(username, claims.getSubject());
+        assertNotNull(issuedAt);
+        assertTrue(issuedAt.before(new Date()));
+        assertNotNull(expiration);
+        assertEquals(username, claims.getSubject());
         assertNotNull(token);
+
     }
 
     @Test
